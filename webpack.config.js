0b02fce55ab188aaa3ec05c9f11a7ce49d0bc863
Wagner Lisboa <wgr.lisboa@gmail.com>
2025-01-17 09:41:47 -0300

    Update webpack configuration to set devServer port to 9001 and disable hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 9002,
+      hot: false,
+    }
   });
 };
